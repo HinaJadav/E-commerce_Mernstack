4b0b5ffdca0cb9ace9e0b3052b54eb556185738e
HinaJadav <hina212002@gmail.com>
2023-08-13 23:22:08 +0530

    2nd Day

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,3 +1,19 @@
+const Product = require("../models/productModel") // impoet "productSchema" from productModel.js
+
+
+//create product
+exports.createProduct = async (req, res) => {
+    const product = await Product.create(req.body);
+
+    res.status(201),json({
+        success:true,
+        product
+    })
+}
+
+
+
+
 exports.getAllProducts = (req,res) => {
     res.status(200).json({message: "Route is working fine."});
 };
---
+++ Backend/models/productModel.js
@@ -0,0 +1,75 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+    name: {
+        type:String,
+        require:[true, "Please Enter Product Name"], // "[true, "Please Enter Product Description"]" --> this is array of values
+        trim:true, // show in "Information.txt"
+    },
+    description: {
+        type:String,
+        require:[true, "Please Enter Product Description"]
+    },
+    price: {
+        type:Number,
+        require:[true, "Please Enter Product Price"],
+        maxLenght: [8, "Price cannot exceed 8 characters"]
+    },
+    rating : {
+        type:Number,
+        default:0
+    },
+    images: [ // more then one image --> make images Array
+        { 
+        // from where we get "public_id"--> when we upload our image on "cloudMemory" , At that time we get "public_id" and "url" for every images
+            public_id: { 
+                type:String,
+                require:true
+            },
+            url: {
+                type:String,
+                require:true
+            }
+        }
+    ],
+    category: {
+        type:String,
+        required:[true, "Please select a category"], 
+        // category choose task we can do in two ways
+        //1) manually
+        //2) using "enum"
+        // but we not do this part here --> we do it in backend
+    },
+    stock: {
+        type:String,
+        require:[true, "Please Enter product stock"],
+        maxLenght:[4, "Stock cannot exceed 4 characters"],
+        default:1
+    },
+    numOfReviews: {
+        type:Number,
+        default:0
+    },
+    reviews: [
+        {
+            name: {
+                type:String,
+                require:true
+            },
+            rating: {
+                type:String,
+                require:true
+            },
+            comment: {
+                type:String,
+                require:true
+            }
+        }
+    ],
+    createdAt: { // when product is created
+        type:Date,
+        default:Date.now
+    }
+})
+
+module.exports = mongoose.model("Product", productSchema); // export productAPI , import in "Contollers"
